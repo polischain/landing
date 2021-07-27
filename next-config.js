8c7ab42cc256818f76969ddd6df970e00055c03f
eabz <eabz@polispay.org>
2021-07-27 18:17:04 -0500

    Base app

---
+++ next-config.js
@@ -0,0 +1,13 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+  images: {
+    domains: ['polis.tech'],
+  },
+  reactStrictMode: true,
+})
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export const Home = (): JSX.Element => (
+  <div className="container">
+    <section>
+      <div className="bg-blue-900" />
+    </section>
+  </div>
+)
+
+export default Home
