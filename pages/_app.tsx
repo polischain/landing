6f2f27ad9bb0e6027bd0e084d4c8ddb0e7b0fc0f
eabz <eabz@polispay.org>
2021-07-27 18:28:08 -0500

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,62 +12,62 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <title key="title">SUSHI</title>
+        <title key="title">Polis Chain</title>
 
         <meta
           key="description"
           name="description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
+          content="Join the payments revolution with our EVM powered network and community"
         />
 
-        <meta name="application-name" content="SUSHI App" />
+        <meta name="application-name" content="Polis Chain" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
-        <meta name="apple-mobile-web-app-title" content="SUSHI App" />
+        <meta name="apple-mobile-web-app-title" content="Polis Chain" />
 
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#F338C3" />
+        <meta name="theme-color" content="# asd" />
 
         <meta key="twitter:card" name="twitter:card" content="app" />
-        <meta key="twitter:title" name="twitter:title" content="SUSHI App" />
+        <meta key="twitter:title" name="twitter:title" content="Polis Chain" />
         <meta
           key="twitter:url"
           name="twitter:url"
-          content="https://app.sushi.com"
+          content="https://polis.tech"
         />
         <meta
           key="twitter:description"
           name="twitter:description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
+          content="Join the payments revolution with our EVM powered network and community"
         />
         <meta
           key="twitter:image"
           name="twitter:image"
-          content="https://app.sushi.com/icons/icon-192x192.png"
+          content="https://polis.tech/icon-192x192.png"
         />
         <meta
           key="twitter:creator"
           name="twitter:creator"
-          content="@SushiSwap"
+          content="@PolisChain"
         />
         <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:site_name" property="og:site_name" content="SUSHI App" />
-        <meta key="og:url" property="og:url" content="https://app.sushi.com" />
+        <meta key="og:site_name" property="og:site_name" content="Polis Chain" />
+        <meta key="og:url" property="og:url" content="https://polis.tech" />
         <meta
           key="og:image"
           property="og:image"
-          content="https://app.sushi.com/apple-touch-icon.png"
+          content="https://polis.tech/apple-touch-icon.png"
         />
         <meta
           key="og:description"
           property="og:description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
+          content="Join the payments revolution with our EVM powered network and community"
         />
       </Head>
       <Component {...pageProps} />
